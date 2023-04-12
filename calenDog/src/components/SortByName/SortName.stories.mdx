04cd88f1b2bce8dd0e3d1e175e230906ed004375
kevin <kevindiegodasilvasousa@gmail.com>
2023-04-12 11:10:12 -0300

    feat: Init files component sort by name

---
+++ calenDog/src/components/SortByName/SortName.stories.mdx
@@ -0,0 +1,1 @@
+import type { Meta, StoryObj } from '@storybook/react';
---
+++ calenDog/src/components/SortByName/SortName.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './SortName.css'
+
+export function SortName() {
+  return (
+    <div className='sortItens'>SortName</div>
+  )
+}
+
