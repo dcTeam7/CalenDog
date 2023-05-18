59d3cac3e9c16168c656f01f66a5844363c152eb
kevin <kevindiegodasilvasousa@gmail.com>
2023-05-17 23:59:38 -0300

    feat: Adding button component

--- calenDog/src/componentes/Button.tsx
+++ calenDog/src/componentes/Button.tsx
@@ -1,26 +1,45 @@
-import React from 'react' 
+import { useState } from 'react'
 import './Button.css'
 
 interface ButtonProps {
-          
-    /**
-     * How large should the button be?
-     */
-    size?: 'small' | 'medium' | 'large';
-    /**
-     * Button contents
-     */
-    label: string;
-    /**
-     * Optional click handler
-     */
-    onClick?: () => void;
-  }
+  /**
+   * Estado boleano do botão
+   */
+  stateBtn: boolean
+  /**
+   * Tamanho do botão
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Conteudo do botão
+   */
+  label: string;
+  /**
+   * Tipo de uso do botão
+   */
+  type?: 'button' | 'submit' | 'reset';
+  /**
+   * Clique opcional
+   */
+  onClick?: (b: boolean) => void;
+}
 
-  function dog() {}
+export function Button(props: ButtonProps) {
 
-  export function Button (props:ButtonProps) {
-    return (
-      <button onClick={props.onClick}> {props.label}</button>
-    )
+  const [clicked, setClicked] = useState(props.stateBtn);
+
+  function handleClick() {
+    setClicked(!clicked)
+    if (props.onClick) {
+      props.onClick(!clicked)
+    }
   }
+  return (
+    <button
+      onClick={handleClick}
+      type={props.type}
+      className={['button', `storybook-button--${props.size}`].join(' ')}>
+      {props.label}
+    </button>
+  )
+}
--- calenDog/src/components/SortByName/SortName.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './SortName.css'
-
-export function SortName() {
-  return (
-    <div className='sortItens'>SortName</div>
-  )
-}
-
